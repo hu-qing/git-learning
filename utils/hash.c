fb3dace96d3b59b3ee21c999013ce38346a0a8a3
xuzj <xuzj@sge.com.cn>
2017-06-16 13:29:58 -0700

    add hash

--- utils/hash.c
+++ utils/hash.c
@@ -1,10 +1,18 @@
 #include "hash.h"
 #include <stdlib.h>
 
+static unsigned int __hash(void *in)
+{
+	return 0;
+}
+
 hash_t *hash_init()
 {
 	hash_t *h = calloc(1, sizeof(hash_t));
 
+	h->bucket_size = DEFAULT_HASH_BUCKET_SIZE;
+	h->bucket = calloc(DEFAULT_HASH_BUCKET_SIZE, sizeof(void *));
+	h->hash = __hash;
 
 	return h;
 }
--- utils/hash.h
+++ utils/hash.h
@@ -4,7 +4,10 @@
 #define DEFAULT_HASH_BUCKET_SIZE 1024
 
 typedef struct hash_s {
-
+	size_t  bucket_size;
+	int     (*cmp)(void *a, void *b);
+	unsigned int hash(void *s);
+	void    *bucket;
 } hash_t;
 
 hash_t *hash_init();
