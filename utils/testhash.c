a94b28002cedc2942f21d9131dafb88593419bae
xuzj <xuzj@sge.com.cn>
2017-06-17 15:00:35 -0700

    add testhash.c

---
+++ utils/testhash.c
@@ -0,0 +1,81 @@
+#include "hash.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    /* Integer test */
+    // begin
+    hash_t *h = hash_init(LL, LL);
+
+    long long key = 5;
+    long long val = 10;
+
+    int i;
+    for (i = 0; i < 1000; ++i) {
+        key = i;
+        val = i + 2;
+        hash_put(h, &key, &val);
+    }
+
+    long long *gval = NULL;
+    int ret;
+    for (i = 0; i < 1000; ++i) {
+        key = i;
+        ret = hash_get(h, &key, (void **)&gval);
+        if (ret != 0)
+            printf("Not found val from hash of key[%lld] error.\n", key);
+        else
+            printf("key[%lld]--val[%lld]\n", key, *gval);
+        gval = NULL;
+    }
+
+    hash_destroy(h);
+
+    // end
+
+    /* String test */
+    /* // begin
+    char s[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    hash_t *h = hash_init_with_cap(STR, STR, 900);
+
+    srandom(time(NULL));
+
+    #define KEY_NUM 3000
+    #define MAXSTRLEN 20
+
+    int str_len;
+    int i;
+    char key[KEY_NUM][MAXSTRLEN + 2];
+    for (i = 0; i < KEY_NUM; ++i) {
+        str_len = random() % MAXSTRLEN + 1;
+        int j;
+        int index;
+        memset(key[i], 0x00, sizeof(key[i]));
+        for (j = 0; j < str_len; ++j) {
+            index = random() % 52;
+            key[i][j] = s[index];
+        }
+
+        hash_put(h, key[i], key[i]);
+    }
+
+    char *gval = NULL;
+    int  ret;
+    for (i = 0; i < KEY_NUM; ++i) {
+        ret = hash_get(h, key[i], (void **)&gval);
+        if (ret != 0)
+            printf("Not found val from hash of key[%s] error.\n", key[i]);
+        else
+            printf("key[%s]--val[%s]\n", key[i], gval);
+        gval = NULL;
+    }
+
+    hash_destroy(h);
+    */ // end
+
+    return 0;
+}
