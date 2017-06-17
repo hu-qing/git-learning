4e3424c5f05547145fbbc2ad6c75334ec57e4b96
xuzj <xuzj@sge.com.cn>
2017-06-16 22:16:42 -0700

    hash

--- utils/hash.c
+++ utils/hash.c
@@ -1,18 +1,69 @@
 #include "hash.h"
 #include <stdlib.h>
 
-static unsigned int __hash(void *in)
+static unsigned int __hash(void *key)
 {
 	return 0;
 }
 
+static unsigned int __str_hash(const char *key)
+{
+	const char     *ptr;
+	unsigned int   val;
+
+	val = 0;
+	ptr = key;
+	while (*ptr != '\0') {
+		unsigned int tmp;
+		val = (val << 4) + (*ptr);
+		if (tmp = (val & 0xf0000000)) {
+			val = val ^(tmp >> 24);
+			val = val ^ tmp;
+		}
+		ptr++;
+	}
+	
+	return val;
+}
+
+static unsigned int __int_hash(const int key)
+{
+	h = capacity * (key * 0.618 mod/*取小数部分*/ 1);
+
+	// such as: 2000 * (6341 * 0.618 mod 1) = 2000 * (3918.738 mod 1) = 2000 * 0.738 = 1476
+}
+
+static int __table_size_for(int cap) {
+        int n = cap - 1;
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+}
+
 hash_t *hash_init()
 {
+	return hash_init_with_cap_and_fact(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+}
+
+hash_t *hash_init_with_cap(int capacity)
+{
+	return hash_init_with_cap_and_fact(capacity, DEFAULT_LOAD_FACTOR);
+}
+
+hash_t *hash_init_with_cap_and_fact(int capacity, float load_factor)
+{
+	if (capacity < 0 || load_factor <= 0)
+		return NULL;
+	
 	hash_t *h = calloc(1, sizeof(hash_t));
 
-	h->bucket_size = DEFAULT_HASH_BUCKET_SIZE;
-	h->bucket = calloc(DEFAULT_HASH_BUCKET_SIZE, sizeof(void *));
+	h->capacity = capacity > MAXIMUM_CAPACITY ? MAXIMUM_CAPACITY : __table_size(capacity);
+	h->factor = load_factor;
 	h->hash = __hash;
+	h->bucket = calloc(capacity, sizeof(void *));
 
 	return h;
 }
--- utils/hash.h
+++ utils/hash.h
@@ -1,15 +1,21 @@
 #ifndef __HASH_H__
 #define __HASH_H__
 
-#define DEFAULT_HASH_BUCKET_SIZE 1024
+#define DEFAULT_INITIAL_CAPACITY (1 << 4)
+#define MAXIMUM_CAPACITY         (1 << 30)
+#define DEFAULT_LOAD_FACTOR      0.75
 
 typedef struct hash_s {
-	size_t  bucket_size;
+	int     capacity;
+	int     size;
+	float   load_factor;
 	int     (*cmp)(void *a, void *b);
-	unsigned int hash(void *s);
+	int     hash(void *s);
 	void    *bucket;
 } hash_t;
 
 hash_t *hash_init();
+hash_t *hash_init_with_cap(int capacity);
+hash_t *hash_init_with_cap_and_fact(int capacity, float load_factor);
 
 #endif
